8323b0facb02fcb7e5e37ac030ed73810f0eda35
wafy <zyalwfie@gmail.com>
2025-02-07 04:39:05 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+    base: '/fund-together/',
+    plugins: [tailwindcss()],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+            },
+        },
+    },
+});
