ff0eb00fca2a3830b0fd76b933c3593b4564fb21
wafy <zyalwfie@gmail.com>
2025-02-10 15:47:07 +0800

    feat: add change password page

--- src/togglePassword.js
+++ src/togglePassword.js
@@ -1,17 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const passwordInput = document.getElementById("password");
-    const showPassword = document.getElementById("show-password");
-    const hidePassword = document.getElementById("hide-password");
+document.addEventListener('DOMContentLoaded', function () {
+	const passwordInput = document.getElementById('password');
+	const showPassword = document.getElementById('show-password');
+	const hidePassword = document.getElementById('hide-password');
 
-    showPassword.addEventListener("click", function () {
-        passwordInput.type = "text";
-        showPassword.classList.add("hidden");
-        hidePassword.classList.remove("hidden");
-    });
+	showPassword.addEventListener('click', function () {
+		passwordInput.type = 'text';
+		showPassword.classList.add('hidden');
+		hidePassword.classList.remove('hidden');
+	});
 
-    hidePassword.addEventListener("click", function () {
-        passwordInput.type = "password";
-        hidePassword.classList.add("hidden");
-        showPassword.classList.remove("hidden");
-    });
+	hidePassword.addEventListener('click', function () {
+		passwordInput.type = 'password';
+		hidePassword.classList.add('hidden');
+		showPassword.classList.remove('hidden');
+	});
 });
